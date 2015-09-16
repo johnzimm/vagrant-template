2476b2753c93e61098ec02de54f6ad2cb7977d07
J.Zimmerman <john.zimm@gmail.com>
2015-09-16 16:14:21 -0700

    Add bootstrap script for puppet agent.  Needs to have ip address for puppet server updated for local env.

---
+++ scripts/bootstrap-puppet_agent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+RELEASEVER=`rpm -qa \*-release | grep -Ei "centos|redhat" | cut -d"-" -f3`
+PUPPETSERVER='ipaddresshere'
+
+case $RELEASEVER in
+[6]*)
+  sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+  ;;
+[7]*)
+  sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+  ;;
+*)
+  echo "Operating System Release $RELEASEVER not supported."
+  echo "I quit."
+  exit 1
+  ;;
+esac
+
+sudo echo "$PUPPETSERVER    puppet" >> /etc/hosts
+
+sudo yum install puppet-agent -y
+sudo /opt/puppetlabs/bin/puppet agent -t
